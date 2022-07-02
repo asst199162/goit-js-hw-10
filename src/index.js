1cc18a2a350966b8c89942c4c3a02f516c04c6f5
asst199162 <asst199162@gmail.com>
2022-07-02 09:39:53 +0300

    func

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 import './css/styles.css';
+const options = '?fields=name,capital,population,flags,languages'
+
+
+
+
+function fetchCountries(name) {
+    return fetch(`https://restcountries.com/v3.1/name/${name}${options}`).then(
+        res => {
+            if (res.ok) {
+                return res.json();
+            }
+            throw new Error(res.statusText)
+        });
+}
+fetchCountries('peru').then(data => console.log(data),)
 
 const DEBOUNCE_DELAY = 300;
+
