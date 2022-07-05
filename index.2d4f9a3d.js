4ca301f0217ffafc7966102e9da97ccef1b776e1
asst199162 <42334307+asst199162@users.noreply.github.com>
2022-07-05 16:07:31 +0000

    Deploying to gh-pages from @ asst199162/goit-js-hw-10@5f8aa038a69c65c45a58ab1e9f16974842ac84db 🚀

--- index.2d4f9a3d.js
+++
@@ -1,2 +0,0 @@
-var e;(e="peru",fetch(`https://restcountries.com/v3.1/name/${e}?fields=name,capital,population,flags,languages`).then((e=>{if(e.ok)return e.json();throw new Error(e.statusText)}))).then((e=>console.log(e)));
-//# sourceMappingURL=index.2d4f9a3d.js.map
--- index.724d583c.js
+++
@@ -1,2 +0,0 @@
-!function(){var n;(n="peru",fetch("https://restcountries.com/v3.1/name/".concat(n).concat("?fields=name,capital,population,flags,languages")).then((function(n){if(n.ok)return n.json();throw new Error(n.statusText)}))).then((function(n){return console.log(n)}))}();
-//# sourceMappingURL=index.724d583c.js.map
